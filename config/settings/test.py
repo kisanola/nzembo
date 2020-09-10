b9b513995f62dbf54e5333726cd05d501fa8ab5f
Espoir Murhabazi <espoir.mur@gmail.com>
2020-09-10 20:25:06 +0200

    - Task Completed : (ref) update docker files and remove white space in config files
    - Next Task :

--- config/settings/test.py
+++ config/settings/test.py
@@ -5,19 +5,13 @@ With these settings, tests run faster.
 from .base import *  # noqa
 from .base import env
 
-# GENERAL
-# ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env(
     "DJANGO_SECRET_KEY",
     default="uJSI9HkvQy4SxKmA6BZJGtOEDCT2SmgGN0C9Vrr8AZdEGwbWlo7B63yTU6C27bLK",
 )
-# https://docs.djangoproject.com/en/dev/ref/settings/#test-runner
+
 TEST_RUNNER = "django.test.runner.DiscoverRunner"
 
-# CACHES
-# ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#caches
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
@@ -25,13 +19,7 @@ CACHES = {
     }
 }
 
-# PASSWORDS
-# ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#password-hashers
 PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
-
-# TEMPLATES
-# ------------------------------------------------------------------------------
 TEMPLATES[-1]["OPTIONS"]["loaders"] = [  # type: ignore[index] # noqa F405
     (
         "django.template.loaders.cached.Loader",
@@ -41,11 +29,7 @@ TEMPLATES[-1]["OPTIONS"]["loaders"] = [  # type: ignore[index] # noqa F405
         ],
     )
 ]
-
-# EMAIL
-# ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
 
-# Your stuff...
-# ------------------------------------------------------------------------------
+
+
