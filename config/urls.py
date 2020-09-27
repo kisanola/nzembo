36f9078eb304d4fd419c3ce0a99ce5f6804c707b
tadomikikuto-bit <lucien.programmer@gmail.com>
2020-09-27 20:34:29 +0300

    - docs: the docs uri swagger -> docs

--- config/urls.py
+++ config/urls.py
@@ -40,8 +40,8 @@ urlpatterns = [
 media_files = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 swagger_urls = [
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^docs(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
