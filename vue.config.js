664e46add158a0fa59a954e85c0d9d95e006f71e
Espoir Murhabazi <espoir.mur@gmail.com>
2020-10-16 02:06:23 +0200

    - Task Completed : (deploy) remove old nginx configurations
    - Next Task :

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
     proxy: {
       "/api*": {
         // Forward frontend dev server request for /api to django dev server
-        target: "http://localhost:8000/",
+        target: "http://80.208.230.111:5000/",
       },
     },
   },
